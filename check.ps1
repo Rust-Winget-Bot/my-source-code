3e4c5a0775c93b0812a5dd3e0fff36539edcf5b9
Jacob Kiesel <kieseljake@live.com>
2022-10-23 13:44:20 -0600

    Port back to windows for GHA

--- check.ps1
+++ check.ps1
@@ -1,21 +1,8 @@
-# This script runs on an ubuntu server, which has the following bootstrap script
-#
-# apt-get update
-# apt-get install -qq wget apt-transport-https software-properties-common
-# wget -q "https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/packages-microsoft-prod.deb"
-# dpkg -i packages-microsoft-prod.deb
-# type -p curl >/dev/null || sudo apt install curl -y
-# curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
-# chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
-# echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
-# apt-get update
-# apt-get install -qq powershell git gh
-# rm packages-microsoft-prod.deb
-# wget -q "https://raw.githubusercontent.com/Rust-Winget-Bot/my-source-code/main/check.ps1"
-# $(which pwsh) -File "check.ps1"
-
+winget install -e --id Git.Git -h --accept-package-agreements --accept-source-agreements
+winget install -e --id GitHub.cli -h --accept-package-agreements --accept-source-agreements
 Install-Module -Name powershell-yaml
 Import-Module powershell-yaml
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User");
 gh auth setup-git
 gh repo clone "Rust-Winget-Bot/winget-pkgs"
 cd winget-pkgs
